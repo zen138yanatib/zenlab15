77dbae7b83f693bd51867dd827db2dd52ce6e806
zen138yanatib <64641188+zen138yanatib@users.noreply.github.com>
2021-02-05 17:07:44 +0700

    Update lab15_1.cpp

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -32,3 +32,40 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *a ,int N ,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << a[i];
+        if(i%M == M-1) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *a , int N , int M){
+    for(int i = 0; i < N*M; i++){
+        a[i] = ((rand()%101 + 1.00)/100)-0.01;
+    }
+}
+
+void findRowSum(const double *a,double *b,int N,int M){
+    cout << fixed << setprecision(2);
+    for (int i=0; i<N; i++){
+        *(b+i) = 0;
+        for(int j=0; j<M; j++){     
+            int x = (i*M) + j;
+            *(b+i) += a[x]; 
+        }        
+    }
+}
+
+void findColSum(const double *a,double *b,int N,int M){
+    cout << fixed << setprecision(2);
+    for (int i=0; i<M; i++){
+        *(b+i) = 0;
+        for(int j=0; j<N; j++){     
+            int x = (j*M) + i;
+            *(b+i) += a[x]; 
+        }        
+    }
+}
